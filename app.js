f4c794f9dcac745bdbc30c5aced88e8ace770152
TalhaJaved7904 <talhamuhammadjaved@gmail.com>
2023-12-15 19:14:49 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,60 @@
+// chapter 1
+alert("First Name")
+alert("Last Name")
+alert("Email")
+alert("Phone Number")
+alert("Password")
+alert("You're Learning JavaScript!")
+alert("Knowledge is of no value unless you put it into practice.")
+
+// Chapter 2
+var firstName = "Talha"
+var nationality
+nationality = "Pakistani"
+
+var teamName = "Ace"
+
+alert(teamName)
+var bestMan = "Charlie"
+bestMan = "Newton"
+
+// chapter 3
+
+var caseQty = 144
+var num = "9"
+var marks = 74
+marks +2
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal)
+
+var value = 58
+value = 58+58
+console.log(value)
+
+// chapter 4
+var productCost = 3.45
+var nameOfBand;
+var topi = 250;
+var firstNameLastName = "Talha Javed"
+// illegal
+// var product cost
+// var "productcost"
+// var alert
+
+// legal
+// var productCost
+// var product_cost
+
+// Chapter 5
+// Modulus operator "%""
+var number = 20%6
+console.log(number)
+var largeNum = 1000*2000
+console.log(largeNum)
+var total = number-largeNum
+console.log(total)
+alert(largeNum)
+
+// Chapter 6
